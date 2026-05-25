c1facb15507391bf73ca09c34bb97e60467b334c
Tyrone-Wilson1611 <TyroneWilson1604@outlook.com>
2026-05-25 14:50:29 +0100

    Update auth.jsx

--- client/src/context/auth.jsx
+++ client/src/context/auth.jsx
@@ -41,40 +41,6 @@ export const AuthProvider = ({ children }) => {
 
 
         return data;import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import Protected from './components/ProtectedRoute.jsx';
-import  Login  from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-import  Home  from './pages/home.jsx';
-import { AuthProvider } from "./context/auth.jsx";
-import Upload from "./pages/Upload.jsx";
-import Profile from "./pages/Profile.jsx";
-import ProfileEdit from './pages/ProfileEdit.jsx';
-
-
-
-const App =() => {
-  return (
-    <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route
-        path = "/"
-        element = { <Protected> <Home /></Protected> }
-        />
-
-        <Route path = "/login" element = {<Login />} />
-        <Route path = "/register" element = {<Register />}/>
-        <Route path= "/upload" element= {<Protected><Upload /></Protected> }/>
-        <Route path = "/users/:username" element={<Protected><Profile /></Protected>}/>
-        <Route path= "/edit-profile" element={<Protected><ProfileEdit/> </Protected>}/>
-        <Route path = "*" element = {<Navigate to = "/" replace />} />
-      </Routes>
-    </AuthProvider>
-    </BrowserRouter>
-  );
-};
-
-export default App;
 
     };
 
